1c8ee0a10f32ecfda04c2d900f06e644a8e17082
shouravrahman <shouravrahman006@gmail.com>
2022-01-24 15:52:48 +0600

    author doc

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,70 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    /* Your types here! */
-  ]),
+	// We name our schema
+	name: 'default',
+	// Then proceed to concatenate our document type
+	// to the ones provided by any plugins that are installed
+	types: schemaTypes.concat([
+		/* Your types here! */
+		{
+			name: 'author',
+			type: 'document',
+			title: 'Author',
+			fields: [
+				{
+					name: 'name',
+					type: 'string',
+					title: 'Name',
+				},
+				{
+					name: 'avatar',
+					type: 'image',
+					title: 'Avatar',
+				},
+			],
+		},
+		{
+			name: 'blog',
+			type: 'document',
+			title: 'Blog',
+			fields: [
+				{
+					name: 'title',
+					type: 'string',
+					title: 'Title',
+				},
+				{
+					name: 'subtitle',
+					type: 'string',
+					title: 'Subtitle',
+				},
+				{
+					name: 'coverImage',
+					type: 'image',
+					title: 'Cover Image',
+				},
+				{
+					name: 'date',
+					type: 'datetime',
+					title: 'Date',
+				},
+				{
+					name: 'author',
+					title: 'Author',
+					type: 'reference',
+					to: [{ type: 'author' }],
+				},
+				{
+					name: 'slug',
+					type: 'slug',
+					title: 'Slug',
+				},
+			],
+		},
+	]),
 })
